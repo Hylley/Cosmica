24d5dbd70fc58b240c2690fac1821ebccddf424b
Hydley F <hydley.jailsonk@gmail.com>
2023-04-10 18:14:24 -0300

    New Fit func (same problems) and ugly lexer implementation
    It technically works, but I want something a little more clean.

--- scanner.cpp
+++ scanner.cpp
@@ -1,52 +1,85 @@
 #include "headers/scanner.h"
 
-bool Fits(const std::string& str, const std::string& model) {
-    size_t strIndex = 0;
-    size_t modelIndex = 0;
-    char lastChar = '\0'; // Track the last non-wildcard character in the model
-
-    while (strIndex < str.size() && modelIndex < model.size()) {
-        if (model[modelIndex] == '%') {
-            modelIndex++;
-            while (strIndex < str.size() && modelIndex < model.size() && str[strIndex] != model[modelIndex]) {
-                strIndex++;
+bool Fits(std::string& str, std::string model) {
+    int i = 0, j = 0, m = str.length(), n = model.length();
+    while (i < m && j < n) {
+        if (model[j] == '%') {
+            // If the % character is the last character in the second string,
+            // then the first string must end at this point for the strings to fit
+            if (j == n - 1) {
+                return true;
             }
-            if (strIndex == str.size()) {
+            // Otherwise, search for the next non-% character in the second string
+            char nextChar = model[j + 1];
+            int k = i;
+            while (k < m && str[k] != nextChar) {
+                k++;
+            }
+            // If the next non-% character is not found in the first string,
+            // then the strings don't fit
+            if (k == m) {
                 return false;
             }
-            lastChar = '\0'; // Reset the last non-wildcard character in the model
-        }
-        else if (str[strIndex] == model[modelIndex]) {
-            strIndex++;
-            modelIndex++;
-            if (model[modelIndex - 1] != '%') {
-                lastChar = model[modelIndex - 1];
+            i = k + 1;
+            j += 2;
+        } else {
+            // If the current characters in the two strings don't match
+            // and the current character in the second string is not a %,
+            // the strings don't fit the pattern
+            if (str[i] != model[j]) {
+                return false;
             }
-        }
-        else {
-            if (lastChar != '\0' && model[modelIndex] != '%') {
-                // If the current model character is the same as the last non-wildcard character
-                // and there is no matching character in the string, return false
-                if (lastChar == model[modelIndex]) {
-                    return false;
-                }
-            }
-            return false;
+            i++;
+            j++;
         }
     }
-
-    while (modelIndex < model.size() && model[modelIndex] == '%') {
-        modelIndex++;
+    // If we have reached the end of the second string, the strings fit the pattern
+    // regardless of the remaining characters in the first string
+    if (j == n) {
+        return true;
     }
-
-    return modelIndex == model.size();
+    return false;
 }
 
 void ScanLine(std::string& line, BlockNode& parent)
 {
+	// IF, ELSE, ELSE-IF
+	if(Fits(line, "se %:"))
+	{
+		std::cout << "IF" << std::endl;
+		return;
+	}else if(Fits(line, "entretanto:"))
+	{
+		std::cout << "ELSE" << std::endl;
+		return;
+	}else if(Fits(line, "entretanto, se %:"))
+	{
+		std::cout << "ELSE-IF" << std::endl;
+		return;
+	}
+
+	// VARIABLE EQUAL ASIGN
 	if(Fits(line, "%==%"))
 	{
-		std::cout << "Funcionou!" << std::endl;
+		std::cout << "EQUAL COMP" << std::endl;
+
+		return;
+	}else if(Fits(line, "%=%"))
+	{
+		std::cout << "EQUAL" << std::endl;
+		return;
+	}
+
+	//FUNCTION DEFINITION AND CALL
+	if(Fits(line, "%(%):"))
+	{
+		std::cout << "FUNC DEF" << std::endl;
+
+		return;
+	}else if(Fits(line, "%(%)"))
+	{
+		std::cout << "FUNC CALL" << std::endl;
+		return;
 	}
 }
 
