b9289a71adcc804ef129e9c342d6316001af1490
Hydley F <hydley.jailsonk@gmail.com>
2023-04-11 02:37:41 -0300

    Whytf was there a tab here?

--- prch.cpp
+++ prch.cpp
@@ -5,7 +5,7 @@
 #include "headers/interpreter.h"
 #include "headers/syntree.h"
 
-int	main(int argc, char	*argv[]) {
+int main(int argc, char	*argv[]) {
 	#if DEBUG_DEVELOPER_FEEDBACK
 	std::cout << "INIT" << std::endl;
 	#endif
