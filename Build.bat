c7b4eba911cd5cb9f51d1febe4dbaa19ac8e66b3
Hydley F <hydley.jailsonk@gmail.com>
2023-04-28 01:01:47 -0300

    Project restructuration + resources
    Now the executable file carry also resource headers (for things like icon, product name, file version and more)

--- Build.bat
+++ Build.bat
@@ -1,12 +1,10 @@
-g++ main.cpp^
-	debug.cpp^
-	scanner.cpp^
-	parser.cpp^
-	interpreter.cpp^
-	utils.cpp^
-	syntree\blocks\*.cpp^
-	syntree\pieces\*.cpp^
-	syntree\statics\*.cpp^
+@echo off
+windres rsc\resources.rc -O coff -o rsc\resources.res
+g++ src\*.cpp^
+	src\syntree\blocks\*.cpp^
+	src\syntree\pieces\*.cpp^
+	src\syntree\statics\*.cpp^
+	rsc\resources.res^
 	-finput-charset=UTF-8^
 	-static^
 	-o cosmica.exe
--- py.py
+++
@@ -1,6 +0,0 @@
-import time
-start_time = time.time()
-
-4 / 4 + 4 * 2 / 5 + 3
-
-print('RUNTIME: {}'.format(time.time() - start_time))
